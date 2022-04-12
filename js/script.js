36f7f7aaa3d2eda1570de4a0c8d122d681fd3184
jtm4 <100496212+jtm4@users.noreply.github.com>
2022-04-12 18:55:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * Create an array of objects each containing a quote, a source, and some containing a citation and/or year. 
 ***/
 const quotes = [
   {
@@ -38,25 +38,33 @@ const quotes = [
     source: "Abraham Lincoln",
   },
   {
-    quote: "Christianity, if false, is of no importance, and if true, of infinite importance. The only thing it cannot be is moderately important.",
-    source: "C.S. Lewis",
+    quote: "Whether you think you can or you think you can't, you're right.",
+    source: "Henry Ford",
   }
 ]
 
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function returns a random element in an array provided as a parameter. 
+ *    
+ * Generates a random number from 0 to the array's length and stores it in the variable `randomIndexNum`.
+ * Uses the `randomIndexNum` variable to select a random array element; stores the element in variable `randomQuoteObject`.
 ***/
 
 function getRandomQuote(array) {
   let randomIndexNum = Math.floor((Math.random() * array.length));
-  let randomQuoteObject = quotes[randomIndexNum];
+  let randomQuoteObject = array[randomIndexNum];
   return randomQuoteObject;
 }
 
 
 /***
- * `printQuote` function
+ * `printQuote` function - sets the HTML equal to the string generated using the random quote
+ * 
+ * Pass `getRandomQuote` function and store the random quote element in a variable
+ * Build a string containing each parameter found in the quote object, and concatenate into a single string.
+ *    Use if statements to test whether the selected quote object contains certain parameters; skip those parameters if they don't exist
+ * Set the html element `quote-box` equal to the value of variable `html` and return the function
 ***/
 
 function printQuote() {
@@ -77,9 +85,6 @@ function printQuote() {
 
   return document.getElementById('quote-box').innerHTML = html;
 }
-
-
-console.log(printQuote(quotes));
 
 
 /***
