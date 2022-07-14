cd47b9bd56130f1dc61cc102e0ab290bea2eda01
tyler-mcdonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-07-13 19:36:08 -0500

    Move quotes array to new file

--- js/script.js
+++ js/script.js
@@ -1,40 +1,3 @@
-/** Array of quotes */
-const quotes = [
-  {
-    quote: "A day wasted on others is not wasted on one's self.",
-    source: 'Charles Dickens',
-    citation: 'A Tale of Two Cities',
-    year: 1859,
-    tags: 'books'
-  },
-  {
-    quote: "No one is useless in this world who lightens the burdens of another.",
-    source: 'Charles Dickens',
-    citation: 'Our Mutual Friend',
-    year: 1865,
-    tags: 'books'
-  },
-  {
-    quote: "There's some good in this world, Mr. Frodo… and it's worth fighting for.",
-    source: 'Samwise Gamgee',
-    citation: 'Lord of the Rings',
-    year: 1954,
-    tags: 'movies'
-  },
-  {
-    quote: "It is better to offer no excuse than a bad one.",
-    source: "George Washington",
-    citation: 'Letter to His Niece',
-    year: 1791,
-    tags: 'presidents'
-  },
-  {
-    quote: "To ease another's heartache is to forget one's own.",
-    source: "Abraham Lincoln",
-    tags: 'presidents'
-  }
-]
-
 
 /**
  * Selects and returns a random quote
@@ -96,4 +59,4 @@ function randomBackgroundColor() {
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
 
 
-setInterval(printQuote, 10000);
+setInterval(printQuote, 7000);
